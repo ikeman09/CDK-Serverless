43ab07783142df3d45562be897544ca04f9ed5b1
Ike Gabriel Yuson <iggyyuson09@gmail.com>
2022-04-02 02:43:38 +0800

    feature--local-invoke

---
+++ functions/getUser.ts
@@ -0,0 +1,27 @@
+export {}
+
+const {format} = require('date-fns')
+exports.handler = async function (event: any) {
+  try{
+    if(event.httpMethod == "GET"){
+      let dt = format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+      return {
+        body: {
+          message: "Hello from root! " + dt
+        }
+      }
+    }
+
+    return {
+      statusCode: 400,
+      headers: {},
+      body: "not GET/ request"
+    }
+  }catch (e){
+    return {
+      statusCode: 400,
+      headers: {},
+      body: JSON.stringify(e.stack || JSON.stringify(e, null, 2))
+    }
+  }
+};
--- lib/cdk_serverless-stack.ts
+++ lib/cdk_serverless-stack.ts
@@ -1,10 +1,52 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import {aws_apigateway, Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkServerlessStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const lambdaLayer = new lambda.LayerVersion(this, "lamdaLayer", {
+      code: lambda.Code.fromAsset('layers'),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_14_X]
+    })
+
+    // const getUser = new lambda.Function(this, "getUser", {
+    //   runtime: lambda.Runtime.NODEJS_14_X,
+    //   code: lambda.Code.fromAsset("functions"),
+    //   handler: "getUser.handler",
+    //   layers: [lambdaLayer]
+    // });
+
+    const getUser = new lambda.Function(this, "getUser", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("functions"),
+      handler: "getUser.handler",
+      layers: [lambdaLayer]
+    })
+
+    const postUser = new lambda.Function(this, "postUser", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("functions"),
+      handler: "postUser.handler",
+      layers: [lambdaLayer]
+    });
+
+    const api = new apigateway.RestApi(this, "sample-api", {
+      restApiName: "AWS CDK template"
+    })
+
+    const getUserIntegration = new apigateway.LambdaIntegration(getUser)
+    const postUserIntegration = new apigateway.LambdaIntegration(postUser)
+
+    const hello = api.root.addResource('hello')
+    hello.addMethod('GET', getUserIntegration)
+
+    const functiontwo = api.root.addResource('functiontwo')
+    functiontwo.addMethod('POST', postUserIntegration)
+    functiontwo.addMethod('GET', postUserIntegration)
 
     // The code that defines your stack goes here
 
