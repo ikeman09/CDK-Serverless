a957d15a9fb85e263e5a5f16667aed044388f100
Ike Gabriel Yuson <iggyyuson09@gmail.com>
2022-04-02 04:06:21 +0800

    feature--try-to-fix-express

--- functions/getUser.ts
+++ functions/getUser.ts
@@ -1,27 +1,61 @@
+import {NextFunction, Request, Response} from "express";
+
 export {}
-
+const express = require('express')
 const {format} = require('date-fns')
-exports.handler = async function (event: any) {
-  try{
-    if(event.httpMethod == "GET"){
-      let dt = format(new Date(2014, 1, 11), 'yyyy-MM-dd')
-      return {
-        body: {
-          message: "Hello from root! " + dt
-        }
-      }
-    }
 
-    return {
-      statusCode: 400,
-      headers: {},
-      body: "not GET/ request"
-    }
-  }catch (e){
-    return {
-      statusCode: 400,
-      headers: {},
-      body: JSON.stringify(e.stack || JSON.stringify(e, null, 2))
-    }
-  }
-};
+const app = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req: Request, res: Response, next: NextFunction) => {
+
+  let dt = format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+
+  return res.status(200).json({
+    message: "Hello from path!" + dt,
+  });
+});
+
+app.post("/hello", (req: Request, res: Response, next: NextFunction) => {
+  return res.status(200).json({
+    message: "Hello POST!",
+  });
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = app;
+
+// exports.handler = async function (event: any) {
+//   try{
+//     if(event.httpMethod == "GET"){
+//       let dt = format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+//       return {
+//         body: {
+//           message: "Hello from root! " + dt
+//         }
+//       }
+//     }
+//
+//     return {
+//       statusCode: 400,
+//       headers: {},
+//       body: "not GET/ request"
+//     }
+//   }catch (e){
+//     return {
+//       statusCode: 400,
+//       headers: {},
+//       body: JSON.stringify(e.stack || JSON.stringify(e, null, 2))
+//     }
+//   }
+// };
